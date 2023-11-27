29f584c8a7f8aede298c0e085596b3a2dd2a53cb
BigyaMaharjan <bigyashadow@gmail.com>
2023-11-27 22:59:28 +0545

    First Commit of JourneyJoy

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using JourneyJoy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JourneyJoy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(CustomerModel model)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JourneyJoy.Models
+{
+    public class CustomerModel
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+
+        public string Password { get; set; }
+        public string MobileNumber { get; set; }
+
+    }
+}
