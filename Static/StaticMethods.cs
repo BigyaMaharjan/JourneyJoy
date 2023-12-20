33a94abf67bd80a832a216f5a1b346e2af982874
sya.sya <prashantmessi08@gmail.com>
2023-12-20 16:58:49 +0545

    encryption | SHINIGAMI

--- Static/StaticMethods.cs
+++ Static/StaticMethods.cs
@@ -1,7 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace JourneyJoy.Static
@@ -27,6 +30,35 @@ namespace JourneyJoy.Static
                 sr = JsonConvert.DeserializeObject<List<T>>(obj);
             }
             return sr;
+        }
+
+        public static string Encryption(string plainText)
+        {
+            string key = "Z947421as8OslwoS788qqw";
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                aes.Mode = CipherMode.ECB;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array, 0, array.Length);
         }
     }
 }
