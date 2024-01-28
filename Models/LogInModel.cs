b45a098c09b6e75bbaca2038d5c8196b5d4e83f1
sya.sya <prashantmessi08@gmail.com>
2024-01-28 16:46:58 +0545

    additional changes | | SHINIGAMI

--- Models/LogInModel.cs
+++ Models/LogInModel.cs
@@ -6,16 +6,12 @@ using System.Web;
 
 namespace JourneyJoy.Models
 {
-    public class LogInModel
+    public class LogInModel : RegisterModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
         [Display(Name = "Username")]
         public string LogInID { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
-        [Display(Name = "Password")]
-        [MaxLength(16, ErrorMessage = "Maximum 16 characters allowed")]
-        [MinLength(8, ErrorMessage = "Minimum 8 characters allowed")]
-        public string password { get; set; }
         public string LogInError { get; set; }
     }
 
@@ -34,5 +30,33 @@ namespace JourneyJoy.Models
         public string Age { get; set; }
         public string DriverLicenceNumber { get; set; }
         public string UserType { get; set; }
+        public string Gender { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets allowed")]
+        public string Firstname { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets allowed")]
+        public string Lastname { get; set; }
+        [Display(Name = "Mobile Number")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Required")]
+        [MaxLength(10, ErrorMessage = "Mobile number should be 10 digit"), MinLength(10, ErrorMessage = "Mobile number should be 10 digit")]
+        [RegularExpression(@"^((980)|(981)|(982)|(984)|(985)|(986)|(974)|(976)|(975)|(988)|(961)|(962)|(972))([0-9])+$", ErrorMessage = "Invalid mobile number")]
+        public string Mobilenumber { get; set; }
+        [Display(Name = "Email Address")]
+        [EmailAddress(ErrorMessage = "Required")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is invalid")]
+        public string Email { get; set; }
+        [Display(Name = "Password")]
+        [MaxLength(16, ErrorMessage = "Maximum 16 characters allowed")]
+        [MinLength(8, ErrorMessage = "Minimum 8 characters allowed")]
+        public string password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [MaxLength(16, ErrorMessage = "Maximum 16 characters allowed")]
+        [MinLength(8, ErrorMessage = "Minimum 8 characters allowed")]
+        public string confirmpassword { get; set; }
     }
 }
